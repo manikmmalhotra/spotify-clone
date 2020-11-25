dee4f198939f481dc7210aa4247f68ead9b9d549
manikmmalhotra <manikmalhotra2113@gmail.com>
2020-11-25 19:06:55 +0530

    done without playing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,81 @@
+import { useEffect } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+import { useDataLayerValue } from './DataLayer';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+    const [{user,token},dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if(_token){
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      })
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        });
+      });
+
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists,
+        });
+      });
+
+      spotify.getPlaylist('37i9dQZEVXcPvEhdBICYiX').then((response) =>
+      dispatch({
+        type: 'SET_DISCOVER_WEEKLY',
+        discover_weekly: response,
+      })
+    );
+
+    spotify.getMyTopArtists().then((response) =>
+    dispatch({
+      type: "SET_TOP_ARTISTS",
+      top_artists: response,
+    })
+  );
+
+  dispatch({
+    type: "SET_SPOTIFY",
+    spotify: spotify,
+  });
+
+
+    }
+  },[]);
+
+  console.log(user);
   return (
     <div className="App">
-     
+      {
+        token ?
+        <Player spotify={spotify} />
+         : 
+          <Login />
+        
+      }   
+
     </div>
   );
 }
 
 export default App;
+
+// 0036899d9332421e9eb19a1421b46195
